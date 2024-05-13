cb48e8775f6365eec63f53df395b7be26b32c54f
Francisco de Assis Zampirolli <fzampirolli@gmail.com>
2024-05-13 09:56:56 -0300

    v5.3

--- teste.py
+++ teste.py
@@ -1,6 +1,10 @@
-import random
+variations = [
+    8, 10, 16, 24, 37, 37, 37, 37, 37, 37, 37, 37, 40, 44, 44, 44, 44, 44, 44, 44, 44, 44,
+    81, 81, 81, 81, 81, 81, 108, 108, 108, 108, 108, 108, 108, 108, 121, 121, 121, 121, 121,
+    121, 184, 184, 184, 184, 184, 184, 184, 184, 184, 184, 184, 184, 184, 188, 188, 188, 188,
+    188, 188, 188, 188, 188, 188, 193, 193, 194, 199, 199, 199, 199
+]
 
-for i in range(10):
-    soma, diferenca = random.sample(range(2, 30), 2)
-    if soma==30 or diferenca == 30:
-        print(soma, diferenca)
+num_elementos_distintos = len(set(variations))
+print("Número de elementos distintos:", num_elementos_distintos)
+print(sorted(set(variations)))
